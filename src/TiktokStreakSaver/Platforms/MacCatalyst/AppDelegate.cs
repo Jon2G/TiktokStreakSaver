0a81bd6ba39139261f02bfc808a52af118d72c1f
Jon2G <jonathan.edu.gar@gmail.com>
2025-12-06 16:01:49 -0600

    Empty project

---
+++ src/TiktokStreakSaver/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TiktokStreakSaver
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
